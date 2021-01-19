e3137ab4d3041bf5234aef584a63922a5e22e7c2
balerum03 <adafer123.angra@gmail.com>
2021-01-19 15:04:51 -0600

    galery has been completed with 4 images

--- src/galery.js
+++ src/galery.js
@@ -10,12 +10,37 @@ const galeryContent = () => {
   const div1 = document.createElement('div');
   const div2 = document.createElement('div');
 
+  const galeryItem1 = document.createElement('img');
+  galeryItem1.src = tacos1;
+  galeryItem1.alt = 'photo1';
+  galeryItem1.classList.add('galeryImage');
 
+  const galeryItem2 = document.createElement('img');
+  galeryItem2.src = tacos2;
+  galeryItem2.alt = 'photo 2';
+  galeryItem2.classList.add('galeryImage');
+
+  const galeryItem3 = document.createElement('img');
+  galeryItem3.src = tacos3;
+  galeryItem3.alt = 'photo 3';
+  galeryItem3.classList.add('galeryImage');
+
+  const galeryItem4 = document.createElement('img');
+  galeryItem4.src = tacos4;
+  galeryItem4.alt = 'photo 4';
+  galeryItem4.classList.add('galeryImage');
+
+  mainGaleryDiv.classList.add('mainGaleryDiv');
+
+  div1.appendChild(galeryItem1);
+  div1.appendChild(galeryItem2);
+  div2.appendChild(galeryItem3);
+  div2.appendChild(galeryItem4);
   mainGaleryDiv.appendChild(div1);
   mainGaleryDiv.appendChild(div2);
   content.appendChild(mainGaleryDiv);
 
-  return galeryDiv;
+  return mainGaleryDiv;
 }
 
 export default galeryContent;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import homePage from './home';
 import navBar from './nav';
 import foodMenu from './menu';
 import aboutUs from './about';
-import './styles/style.css'
+import galeryContent from './galery';
+import './styles/style.css';
 
 navBar();
 homePage();
 foodMenu();
 aboutUs();
+galeryContent();
